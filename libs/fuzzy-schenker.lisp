bd82c63da29cf259169562fb15461ce2c6463494
Jonathan Stuart-Moore <jstuartm@gmail.com>
2012-07-05 22:04:10 -0700

    started writing rules for schenker line matching

---
+++ libs/fuzzy-schenker.lisp
@@ -0,0 +1,24 @@
+(defun note-is-scale-degree-n (cope-event n)
+  "Check to see if note is of scale degree n"
+  (equal (midi-pitch-to-pitch-class (second cope-event)) n))
+
+(defun note-is-harmonized-by-root-n (cope-event root)
+  "Returns 1 if note is harmonized by root n"
+  )
+
+(defun note-is-near-beginning (cope-event)
+  "Fuzzy function, returns 1 if note is near beginning.")
+
+(defun note-is-near-end (cope-event)
+  "Fuzzy function, returns 1 if note is near the end.")
+
+(defun note-is-near-top-of-range (cope-event)
+  "Fuzzy function, returns 1 if note is near top of range.")
+
+(defun note-is-top-voice (cope-event)
+  "Fuzzy function, returns 1 if note is in (or near) top voice.")
+
+(defun note-is-harmonized (cope-event)
+  "Fuzzy function, returns 1 if note is properly harmonized within the key.")
+
+(defun 
