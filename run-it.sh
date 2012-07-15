3b2d48feda5cb6cde406660ff5fee51e876d77bf
Jonathan Stuart-Moore <jstuartm@gmail.com>
2012-07-14 23:44:55 -0600

    UPDATE: removed kickoff of lisp script since this script now simply combines output files previously generated by lisp (user runs lisp). Also added more status printouts

--- run-it.sh
+++ run-it.sh
@@ -12,7 +12,8 @@ LILY_POND_TEMPLATES=$BASE_PATH/project-v2/templates
 LILY_POND_EXECUTABLE=/Applications/LilyPond.app/Contents/Resources/bin/lilypond
 PREVIEW_APP=/Applications/Preview.app
 PHP_REPLACER=$BASE_PATH/project-v2/replacer.php
-LISP_SCRIPT=$BASE_PATH/project-v2/write-lily-pond-files.lisp
+LISP_TEST_SCRIPT=$BASE_PATH/project-v2/write-lily-pond-files.lisp
+LISP_SCRIPT=$BASE_PATH/project-v2/start-2.lisp
 
 # FILE PATHS
 USE_LILY_POND_TEMPLATE=$LILY_POND_TEMPLATES/chord-analysis-template.ly
@@ -21,20 +22,33 @@ LILY_POND_SOURCE=$TMP_DIR/analysis.ly
 LILY_POND_OUTPUT=$OUTPUT_DIR
 
 
+echo
 echo "Cleaning the Temp Directory..."
-rm $TMP_DIR/*
+echo "------------------------------------------------------------"
+#rm $TMP_DIR/*
 
+echo
 echo "Running Lisp Code..."
-$LISP_SCRIPT
+echo "------------------------------------------------------------"
+#$LISP_TEST_SCRIPT
+#$LISP_SCRIPT
 
+echo
 echo "Copying Template to Temp Directory..."
+echo "------------------------------------------------------------"
 cp $USE_LILY_POND_TEMPLATE $LILY_POND_WORKING_TEMPLATE
 
+echo
 echo "Filling in Template..."
+echo "------------------------------------------------------------"
 php $PHP_REPLACER
 
+echo
 echo "Running Lily Pond..."
+echo "------------------------------------------------------------"
 $LILY_POND_EXECUTABLE -o $LILY_POND_OUTPUT $LILY_POND_SOURCE
 
+echo
 echo "Launching PDF..."
+echo "------------------------------------------------------------"
 open -a $PREVIEW_APP $LILY_POND_OUTPUT/analysis.pdf
